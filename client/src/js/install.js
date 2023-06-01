df5fa7e2a07e70a2bb01a4598f51ee5cc1ee0fa5
Milywayz <jtjenks@gmail.com>
2023-05-31 19:07:07 -0500

    added database code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,4 +8,8 @@ window.addEventListener('beforeinstallprompt', (event) => {});
 butInstall.addEventListener('click', async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = 'Successfully installed!';
+    console.log('👍', 'appinstalled', event);
+  });
+  
